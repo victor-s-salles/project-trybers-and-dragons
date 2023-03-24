6b416f58be91a2f09573d8e76c2414b63023f6c0
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-03-24 17:15:24 -0300

    Correção no requisito 7 e feito o requisito 8

--- src/Character.ts
+++ src/Character.ts
@@ -50,7 +50,7 @@ export default class Character implements Fighter {
   }
 
   receiveDamage(attackPoints: number): number {
-    const damage = this._defense - attackPoints;
+    const damage = attackPoints - this._defense;
     if (damage > 0) {
       this._lifePoints -= damage;
     }
@@ -66,5 +66,5 @@ export default class Character implements Fighter {
   get strength():number { return this._strength; }
   get defense():number { return this._defense; }
   get dexterity():number { return this._dexterity; }
-  get energy(): Energy { return this._energy; }
+  get energy(): Energy { return { ...this._energy }; }
 }
