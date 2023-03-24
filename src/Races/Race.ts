a86890d7adcd3e4fe97c492aac13ec2a3a4c8885
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-03-24 14:51:08 -0300

    Requisito 1

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,23 @@
-export default abstract class Race {}
+export default abstract class Race {
+  readonly _name: string;
+  readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
---
+++ src/Races/index.ts
@@ -0,0 +1,3 @@
+import Race from './race';
+
+export default Race;
