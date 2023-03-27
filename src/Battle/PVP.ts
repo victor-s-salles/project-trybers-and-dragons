fc0a5e320ede7c10d29ab25193a3104246ff4778
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-03-27 15:22:18 -0300

    Requisito 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+export default class PVP extends Battle {
+  private _playerOne: Fighter;
+  private _playerTwo: Fighter;
+
+  constructor(playerOne: Fighter, playerTwo: Fighter) {
+    super(playerOne);
+    this._playerOne = playerOne;
+    this._playerTwo = playerTwo;
+  }
+
+  fight(): number {
+    while (this._playerOne.lifePoints > 0 && this._playerTwo.lifePoints > 0) {
+      this._playerOne.attack(this._playerTwo);
+      this._playerTwo.attack(this._playerOne);
+    }
+    return this._playerOne.lifePoints === -1 ? -1 : 1;
+  }
+}
